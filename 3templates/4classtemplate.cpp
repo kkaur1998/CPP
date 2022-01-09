69ac3bf957111b958399a0f9f69126f7da8ed5c9
Kamaljeet Kaur <53866651+kkaur1998@users.noreply.github.com>
2022-01-09 18:00:03 +0530

    Update 4classtemplate.cpp

--- 3templates/4classtemplate.cpp
+++ 3templates/4classtemplate.cpp
@@ -6,13 +6,11 @@ class Pair{
     public:
     T x,y;
     Pair (T i, T j){x=i;y=j;}    //constructor
-    T getFirst();
-    T getSecond();
+    T getFirst(){return x;};
+    T getSecond(){return y;};
 };
 
 template <typename T>
-Pair<T>::getFirst(){return x;}
-Pair<T>::getSecond(){return y;}
 int main(){
     Pair <int> p1(10,20),p2(30,40);
     cout<<p1.getFirst()<<"     "<<p1.getFirst();<<endl;
